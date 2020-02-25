f736bc0f7c98f52a2e542264458ac8647387ca17
Ayden-James Nolan <aj.nolan@sencom.com.au>
2020-02-25 10:14:19 +0800

    - Restructed source code directory and defined assemblies to demonstrate understandaning and application of assembly definition and usage.

---
+++ Assets/Source/Tests/Reflection/FieldReflectorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using UnityEngine;
+
+
+namespace StudioEntropy.Demo.Tests
+{
+    [ TestFixture ]
+    public class FieldReflectorTests
+    {
+
+        private class Person
+        {
+
+            private Guid id;
+
+            private List< string > tags;
+            
+            public Person( )
+            {
+                id = Guid.NewGuid( );
+            }
+            
+        }
+        
+    }
+
+}
+
